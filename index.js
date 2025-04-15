002632b26ba09c73b600072e4893c8a687b68c45
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-04-15 22:15:13 +0600

    get a single doctor api created

--- index.js
+++ index.js
@@ -51,9 +51,25 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/users', verifyToken, verifyToken, async (req, res) => {
+        app.get('/users', async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
+        });
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
         });
 
         app.get('/doctors', async (req, res) => {
