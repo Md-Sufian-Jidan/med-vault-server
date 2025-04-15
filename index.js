b13a0353c4d1009ee098b55b301ab6c253c50920
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-04-15 20:28:41 +0600

    basic server setup added

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.port || 5000;
+
+const options = {
+    origin: [
+        'http://localhost:5173',
+    ],
+    credentials: true
+};
+
+//middleware
+app.use(cors(options));
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvjjrvn.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const userCollection = client.db('medVault').collection('users');
+        const doctorCollection = client.db('medVault').collection('doctors');
+        const reviewCollection = client.db('medVault').collection('reviews');
+
+        //users related apis
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // insert email if user doesnt exists: 
+            // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.get('/doctors', async (req, res) => {
+            const result = await doctorCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get('/view/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await doctorCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('MED VAULT is ready for deploy');
+});
+
+app.listen(port, (req, res) => {
+    console.log(`MED VAULT is running on port: ${port}`);
+});
