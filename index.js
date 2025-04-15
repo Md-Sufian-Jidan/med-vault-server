a845b3e0c2ef90fa64c3e3df2e550fae8cb5eb63
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-04-15 21:45:33 +0600

    get the reviews api created

--- index.js
+++ index.js
@@ -67,6 +67,11 @@ async function run() {
             const result = await reviewCollection.find().toArray();
             res.send(result);
         });
+        
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
